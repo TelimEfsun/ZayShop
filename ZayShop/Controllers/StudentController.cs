3610979662d076133ef06862e827d4f7c4041389
EfsunYusubov <yusubzade2025@mail.ru>
2025-07-28 22:50:23 +0400

    Student Crud Operation

--- ZayShop/Controllers/StudentController.cs
+++ ZayShop/Controllers/StudentController.cs
@@ -46,5 +46,81 @@ namespace ZayShop.Controllers
         }
 
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StudentModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Studentss.Add(student);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        //Get student/edit/1
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Studentss.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(StudentModel student, int id)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Studentss.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Studentss.FirstOrDefaultAsync(m=>m.Id==id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Studentss.FindAsync(id);
+            if (student != null)
+            {
+                _context.Studentss.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
